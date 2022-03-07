bbac67d416df8b64b936c0a4f6cd5001cc4286c3
Cubites <cubite404@gmail.com>
2022-03-07 23:02:32 +0900

    homework
    homework

--- HTML/web/doum/doum_slide/js/custom copy.js
+++ HTML/web/doum/doum_slide/js/custom copy.js
@@ -10,26 +10,25 @@ var num = 0;
 
 function slideMove(n){
     if(n == 0){
+        var cont = document.getElementsByClassName("cont");
         num--;
         if(num < 0){
-            document.getElementsByClassName("cont")[num+1].style.display = "none";
+            cont[num+1].style.display = "none";
             num = 4;
-            document.getElementsByClassName("cont")[num].style.display = "block";
+            cont[num].style.display = "block";
         }else{
-            document.getElementsByClassName("cont")[num+1].style.display = "none";
-            document.getElementsByClassName("cont")[num].style.display = "block";
+            cont[num+1].style.display = "none";
+            cont[num].style.display = "block";
         }
     }else{
         num++;
         if(num > 4){
-            document.getElementsByClassName("cont")[num-1].style.display = "none";
+            cont[num-1].style.display = "none";
             num = 0;
-            document.getElementsByClassName("cont")[num].style.display = "block";
+            cont[num].style.display = "block";
         }else{
-            document.getElementsByClassName("cont")[num-1].style.display = "none";
-            document.getElementsByClassName("cont")[num].style.display = "block";
+            cont[num-1].style.display = "none";
+            cont[num].style.display = "block";
         }
     }
-
-    document.getElementsByClassName("cont")
 }
--- homework/timer/js/script.js
+++ homework/timer/js/script.js
@@ -1,6 +1,22 @@
 var sec = -30;
 
-var start = setInterval(function(){
+var timer = setInterval(time, 1000);
+
+function time(){
     sec++;
     document.getElementById("pin").style.transform = "rotate(" + (6*sec) + "deg)";
-}, 1000);
+    document.getElementById("seconds").value = (sec + 30);
+}
+function pause(){
+    clearInterval(timer);
+}
+
+function start(){
+    timer = setInterval(time, 1000);
+}
+function stop(){
+    clearInterval(timer);
+    sec = -30;
+    document.getElementById("pin").style.transform = "rotate(" + (6*sec) + "deg)";
+    document.getElementById("seconds").value = 0;
+}
