08162cac6558dbf03d7b5fdfbc8973163b6f7584
Cubites <cubite404@gmail.com>
2022-04-06 16:34:50 +0900

    0406
    android studio, react

---
+++ mobileApp/rps/www/js/custom.js
@@ -0,0 +1,66 @@
+'use strict';
+let button = document.querySelectorAll('div.buttons button');
+let img = document.querySelectorAll('div.buttons img');
+let result = document.querySelectorAll('div.main img');
+let resultT = document.getElementById('result');
+let hands = ["rock", "paper", "scissors"];
+let totalResult = document.querySelectorAll('.totalResult tr td');
+
+
+img.forEach((button) => {
+    button.addEventListener('touchstart', () => {
+        button.classList += " click";
+    })
+    button.addEventListener('touchend', () => {
+        button.classList.remove("click");
+        let user = button.alt;
+        let com = hands[Math.floor(Math.random() * 3)];
+        result[0].src = `images/${user}.png`;
+        result[0].alt = user;
+        
+        result[1].src = `images/${com}.png`;
+        result[1].alt = com;
+
+        result[0].animate([
+            {left: '-160px'},
+            {left: '0'}
+        ],{
+            duration: 1000,
+            fill: 'forwards',
+            easing: 'ease-in-out'
+        })
+
+        result[1].animate([
+            {right: '-160px'},
+            {right: '0'}
+        ],{
+            duration: 1000,
+            fill: 'forwards',
+            easing: 'ease-in-out'
+        })
+        resultText(user, com);
+    })
+})
+
+document.getElementById("reset").addEventListener('touchend', () => {
+    for(let i of totalResult){
+        i.innerHTML = '0';
+    }
+});
+
+function resultText(user, com){
+    let userIndex = hands.indexOf(user);
+    let comIndex = hands.indexOf(com);
+    if(userIndex > comIndex || (userIndex == 0 && comIndex == 2)){
+        resultT.innerHTML = "유저 승!";
+        resultT.style.background = "skyblue";
+        totalResult[0].innerHTML = Number(totalResult[0].innerHTML) + 1;
+    }else if(comIndex > userIndex || (userIndex == 2 && comIndex == 0)){
+        resultT.innerHTML = "유저 패...";
+        resultT.style.background = "firebrick";
+        totalResult[1].innerHTML = Number(totalResult[1].innerHTML) + 1;
+    }else{
+        resultT.innerHTML = "무승부";
+        resultT.style.background = "yellowgreen";
+    }
+}
