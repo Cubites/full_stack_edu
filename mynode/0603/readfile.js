98581ed02ef565f3101fd47d22080dbe20d6b101
Cubites <cubite404@gmail.com>
2022-07-06 17:01:45 +0900

    0704

--- mynode/0603/readfile.js
+++ mynode/0603/readfile.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 
 // readFileSync => 동기식 IO
 let mypackage = fs.readFileSync('./package.json', 'utf8');
+console.log(typeof(mypackage));
 console.log(mypackage);
 console.log('package.json 파일을 읽었습니다.');
